597a29e4ab0d8dd6f28f156d05b30978f27231c1
MintBloom <a.stegmann09@gmail.com>
2025-06-30 22:27:49 +0100

    .png images added.
    Further buttons created in program code and README started.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Moosix-player
-Audio file player application
+Audio file player application. As of the making of this project I am currently using Fedora Linux which does not have a great default media player, so I have tried to make my own one!
+
+## Application Window
+
+## Inner Workings
+
+
+## Challenges
--- music_player.py
+++ music_player.py
@@ -8,4 +8,29 @@ root.geometry("600x360") #window startup size in pixels
 
 pygame.mixer.init() #allows audio to played
 
+audio_file_list = Listbox(root, bg="black", fg="white", width=100, height=16)
+audio_file_list.pack()
+
+#files used for corresponding commands in audio player
+img_play_bttn = PhotoImage(file="Images/Play.png")
+img_pause_bttn = PhotoImage(file="Images/Pause.png")
+img_next_bttn = PhotoImage(file="Images/Next.png")
+img_previous_bttn = PhotoImage(file="Images/Previous.png")
+
+#tkinter frames which section the application window
+control_frame = Frame(root)
+control_frame.pack() 
+
+#buttons
+play_bttn = Button(control_frame, image=img_play_bttn, borderwidth=0)
+pause_bttn = Button(control_frame, image=img_pause_bttn, borderwidth=0)
+next_bttn = Button(control_frame, image=img_next_bttn, borderwidth=0)
+previous_bttn = Button(control_frame, image=img_previous_bttn, borderwidth=0)
+
+#actual buttons on the application window
+play_bttn.grid(row=0, column=1, padx=1, pady=0)
+pause_bttn.grid(row=0, column=2, padx=1, pady=0)
+next_bttn.grid(row=0, column=3, padx=1, pady=0)
+previous_bttn.grid(row=0, column=0, padx=1, pady=0)
+
 root.mainloop()
